39700ae29e4a3640682c11c15916692f1eb5a161
Mitchell Day <mitchell.day.6@gmail.com>
2021-08-23 22:46:39 +1000

    Added functionality to DistMonitor class

--- main/src/DistMonitor/DistMonitor.cpp
+++ main/src/DistMonitor/DistMonitor.cpp
@@ -1,16 +1,38 @@
 #include "DistMonitor.h"
+#include "../Car/Car.h"
 #include "Arduino.h"
 
-DistMonitor::DistMonitor(int trigPin, int echoPin){
+DistMonitor::DistMonitor(int trigPin, int echoPin, int distThreshold, Car car)
+{
     _trigPin = trigPin;
     _echoPin = echoPin;
+    _car = car;
+    _distThreshold = distThreshold;
+    pinMode(_trigPin, OUTPUT);
+    pinMode(_echoPin, INPUT);
 };
 
-void DistMonitor::checkDist(){
-
+bool DistMonitor::checkDist(int distance){
+    return distance < _distThreshold;
 };
+    
 
-float DistMonitor::getCurDist()
+int DistMonitor::getCurDist()
 {
+    // Clears the trigPin condition
+    digitalWrite(_trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(_trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(_trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(_echoPin, HIGH);
+    // Calculating the distance
+    curDist = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    // Displays the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
     return curDist;
 };
--- main/src/DistMonitor/DistMonitor.h
+++ main/src/DistMonitor/DistMonitor.h
@@ -2,16 +2,21 @@
 #ifndef DistMonitor_h
 #define DistMonitor_h
 
+#include "../Car/Car.h"
+
 class DistMonitor {
   private:
-    float curDist;
+    int curDist; //the distance the sensor returns
+    int _distThreshold; //the distance at which to stop car
+    long duration;
     int _trigPin;
     int _echoPin;
+    Car _car;
     
   public:
-    DistMonitor(int trigPin, int echoPin);
-    void checkDist();
-    float getCurDist();
+    DistMonitor(int trigPin, int echoPin, int distThreshold, Car car);
+    bool checkDist(int distance);
+    int getCurDist();
   
 };
 

227206843c01d8127985353ebcee226603113ac5
Mitchell Day <mitchell.day.6@gmail.com>
2021-08-23 22:34:08 +1000

    add dist monitor setup in main ino

--- main/main.ino
+++ main/main.ino
@@ -4,7 +4,8 @@
 #include "src/DistMonitor/DistMonitor.h"
 
 Car car(4,5,6,7);
-AudioCapture aCapture = AudioCapture(A0, A1, A2);
+AudioCapture aCapture(A0, A1, A2);
+DistMonitor distMonitor(2,3);
 
 void setup()
 {

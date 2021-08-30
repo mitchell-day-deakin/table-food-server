85e9b5907a318d6a16c1a0ab0c183d4bb5da535a
Mitchell Day <mitchell.day.6@gmail.com>
2021-08-30 23:18:48 +1000

    bt and more

--- main/main.ino
+++ main/main.ino
@@ -2,14 +2,22 @@
 #include "src/Car/Motor.h"
 #include "src/AudioCapture/AudioCapture.h"
 #include "src/DistMonitor/DistMonitor.h"
+#include <AltSoftSerial.h>
+AltSoftSerial btSerial;
 
 Car car(4,5,6,7);
 AudioCapture aCapture(A0, A1, A2);
 DistMonitor distMonitor(2,3, 16);
 
+//Bluetooth pins
+int rxPin;
+int txPin;
+
 void setup()
 {
   Serial.begin(9600);
+  btSerial.begin(9600);
+  //pinMode(txPin, OUTPUT);
 }
 
 //test the audio system
@@ -17,7 +25,7 @@ void audioTest()
 {
   //test the audio system
   bool isTriggered = aCapture.readMics();
-  //delay(1000);
+  delay(1000);
   if (isTriggered)
   {
     int degrees = aCapture.getAudioDirection();
@@ -41,6 +49,16 @@ void testAudio(){
 
 }
 
+//check bluetooth board;
+
+void btRead(){
+  if(btSerial.available()){
+    char btValue = btSerial.read();
+    Serial.println("BT Input");
+    Serial.println(btValue);
+  }
+}
+
 
 //test distance monitor
 void testDistMonitor(){
@@ -54,5 +72,6 @@ void loop()
   //testCar();
   //testDistMonitor();
   audioTest();
+  btRead();
   
 }
--- main/src/AudioCapture/AudioCapture.h
+++ main/src/AudioCapture/AudioCapture.h
@@ -5,12 +5,12 @@ And also the direction the audio came from.
 #ifndef AudioCapture_h
 #define AudioCapture_h
 
+#include "../Car/Car.h"
 
 class AudioCapture {
     private:
         int TRIG_VAL = 690; // the threshold for a successful clap
         int AUDIO_DIR = -1; //this will store the audio direction heard from clap
-
         int counter = 0; //counts from first clap
         bool clap1;
         bool clap2;
